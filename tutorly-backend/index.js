2460c496403ae16b4b42ea12ee6407df55e6e671
rayeemomayeer <rayeemomayeer@gmail.com>
2026-02-05 05:21:24 +0600

    initialized node, express

---
+++ tutorly-backend/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 8000
+
+app.get('/', (req, res) => {
+  res.send('Server is running')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
